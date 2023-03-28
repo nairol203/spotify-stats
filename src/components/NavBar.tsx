9dd393d672742ee7ddfbf980be9d11c281082dcc
nairol203 <florian@nairol.me>
2023-03-28 13:51:27 +0200

    enhanced hover effect

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -11,28 +11,25 @@ export default function NavBar() {
 		<nav className='background-blur-lg my-4 ml-4 hidden flex-col gap-4 overflow-y-auto rounded-lg bg-white/25 py-4 px-3 shadow-md md:flex'>
 			<Link href='/' className='flex items-center gap-2'>
 				{
-                    // @ts-expect-error
-                    <FontAwesomeIcon icon={faSpotify} height={35} width={35} />
-                }
+					// @ts-expect-error
+					<FontAwesomeIcon icon={faSpotify} height={35} width={35} />
+				}
 				<h1>Spotify Stats</h1>
 			</Link>
-			<div>
-				<Link className={`${router.pathname === '/' ? '' : 'text-gray-200 hover:text-white'} flex items-center gap-4 p-2`} href='/'>
+			<div className='grid gap-0.5'>
+				<Link className={`${router.pathname === '/' ? 'bg-white/25' : 'hover:bg-white/25'} flex items-center gap-4 rounded p-2`} href='/'>
 					<FontAwesomeIcon icon={faHome} height={20} width={20} />
 					<span>Home</span>
 				</Link>
-				<Link
-					className={`${router.pathname === '/recently-played' ? '' : 'text-gray-200 hover:text-white'} flex items-center gap-4 p-2`}
-					href='/recently-played'
-				>
+				<Link className={`${router.pathname === '/recently-played' ? 'bg-white/25' : 'hover:bg-white/25'} flex items-center gap-4 rounded p-2`} href='/recently-played'>
 					<FontAwesomeIcon icon={faClockRotateLeft} height={20} width={20} />
 					<span>Recently Played</span>
 				</Link>
-				<Link className={`${router.pathname === '/top/artists' ? '' : 'text-gray-200 hover:text-white'} flex items-center gap-4 p-2`} href='/top/artists'>
+				<Link className={`${router.pathname === '/top/artists' ? 'bg-white/25' : 'hover:bg-white/25'} flex items-center gap-4 rounded p-2`} href='/top/artists'>
 					<FontAwesomeIcon icon={faChartLine} height={20} width={20} />
 					<span>Top Artists</span>
 				</Link>
-				<Link className={`${router.pathname === '/top/tracks' ? '' : 'text-gray-200 hover:text-white'} flex items-center gap-4 p-2`} href='/top/tracks'>
+				<Link className={`${router.pathname === '/top/tracks' ? 'bg-white/25' : 'hover:bg-white/25'} flex items-center gap-4 rounded p-2`} href='/top/tracks'>
 					<FontAwesomeIcon icon={faChartLine} height={20} width={20} />
 					<span>Top Tracks</span>
 				</Link>
