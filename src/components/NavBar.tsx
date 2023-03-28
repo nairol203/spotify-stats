c4b4aa0f248a2dc7e9f912c53b1b92e772c4ba8e
Florian Bock <f.bock.2003@gmail.com>
2023-03-28 15:33:21 +0200

    custom login page + overall tweaks

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -9,14 +9,17 @@ export default function NavBar() {
 
 	return (
 		<nav className='background-blur-lg my-4 ml-4 hidden flex-col gap-4 overflow-y-auto rounded-lg bg-white/25 py-4 px-3 shadow-md md:flex'>
-			<Link href='/' className='flex items-center gap-2'>
+			<Link href='/' className='flex items-center gap-3'>
 				{
 					// @ts-expect-error
-					<FontAwesomeIcon icon={faSpotify} height={35} width={35} />
+					<FontAwesomeIcon icon={faSpotify} height={50} width={50} />
 				}
-				<h1>Spotify Stats</h1>
+				<h1 className='grid'>
+					Nairol
+					<span className='text-lg'>Spotify Stats</span>
+				</h1>
 			</Link>
-			<div className='grid gap-0.5'>
+			<div>
 				<Link className={`${router.pathname === '/' ? 'bg-white/25' : 'hover:bg-white/25'} flex items-center gap-4 rounded p-2`} href='/'>
 					<FontAwesomeIcon icon={faHome} height={20} width={20} />
 					<span>Home</span>
@@ -34,6 +37,9 @@ export default function NavBar() {
 					<span>Top Tracks</span>
 				</Link>
 			</div>
+			<a href='https://nairol.me' target='_blank' rel='noreferrer' className='mt-auto flex justify-center text-sm hover:underline'>
+				© 2023 nairol203
+			</a>
 		</nav>
 	);
 }
