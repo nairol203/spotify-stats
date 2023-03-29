304ff165e726bfdb16375a146d9d7229800967a3
Florian Bock <f.bock.2003@gmail.com>
2023-03-29 16:52:29 +0200

    reverting back to blank imprint

--- src/pages/imprint.tsx
+++ src/pages/imprint.tsx
@@ -1,16 +1,17 @@
 export default function Imprint() {
 	return (
-		<div className="grid gap-4 rounded-xl bg-white/25 p-6 shadow-md backdrop-blur-lg">
-			<h1>Imprint</h1>
-			<span>
-				Florian Bock <br />
-				Heischberg 12 <br />
-				24119 Kronshagen
-			</span>
-			<span>
-				Phone: +49 1575 2872869 <br />
-				Email: <a href="mailto:info@nairol.me">info@nairol.me</a>
-			</span>
-		</div>
+		<></>
+		// <div className="grid gap-4 rounded-xl bg-white/25 p-6 shadow-md backdrop-blur-lg">
+		// 	<h1>Imprint</h1>
+		// 	<span>
+		// 		Florian Bock <br />
+		// 		Heischberg 12 <br />
+		// 		24119 Kronshagen
+		// 	</span>
+		// 	<span>
+		// 		Phone: +49 1575 2872869 <br />
+		// 		Email: <a href="mailto:info@nairol.me">info@nairol.me</a>
+		// 	</span>
+		// </div>
 	);
 }
