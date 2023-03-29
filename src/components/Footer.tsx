41ad99c66c17ec60c03263f48b125fdffae6b8b9
nairol203 <florian@nairol.me>
2023-03-29 16:57:12 +0200

    Imprint & Privacy

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,20 @@
+import Link from 'next/link'
+
 export default function Footer() {
 	return (
-		<footer className='flex justify-center p-4 md:hidden'>
-			<a href='https://nairol.me' target='_blank' rel='noreferrer' className='text-sm'>
+		<footer className='grid justify-center p-4 md:hidden'>
+			<a href='https://nairol.me' target='_blank' rel='noreferrer' className='text-sm hover:underline '>
 				© 2023 nairol203
 			</a>
+			<div className='flex gap-1'>
+				<Link className='hover:underline' href='/imprint'>
+					Imprint
+				</Link>
+				<span>•</span>
+				<Link className='hover:underline' href='/privacy'>
+					Privacy
+				</Link>
+			</div>
 		</footer>
 	);
 }
