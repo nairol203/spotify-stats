929b5f8cf848745a67a9328bb57826349b25cdf9
nairol203 <florian@nairol.me>
2023-03-29 17:07:43 +0200

    no hover for mobile

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,19 +1,15 @@
-import Link from 'next/link'
+import Link from 'next/link';
 
 export default function Footer() {
 	return (
 		<footer className='grid justify-center p-4 md:hidden'>
-			<a href='https://nairol.me' target='_blank' rel='noreferrer' className='text-sm hover:underline '>
+			<a href='https://nairol.me' target='_blank' rel='noreferrer' className='text-sm '>
 				© 2023 nairol203
 			</a>
 			<div className='flex gap-1'>
-				<Link className='hover:underline' href='/imprint'>
-					Imprint
-				</Link>
+				<Link href='/imprint'>Imprint</Link>
 				<span>•</span>
-				<Link className='hover:underline' href='/privacy'>
-					Privacy
-				</Link>
+				<Link href='/privacy'>Privacy</Link>
 			</div>
 		</footer>
 	);
