bc943b826db167da9ab8982fa970e50d13839093
nairol203 <florian@nairol.me>
2025-01-02 23:21:00 +0100

    Updated copyright to 2025

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
 	return (
 		<footer className='grid justify-center p-4 md:hidden'>
 			<a href='https://nairol.me' target='_blank' rel='noreferrer' className='text-sm '>
-				© 2023 nairol203
+				© 2025 nairol203
 			</a>
 			<div className='flex gap-1'>
 				<Link href='/imprint'>Imprint</Link>
