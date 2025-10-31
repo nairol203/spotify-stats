c4d70ae374d7c39f76b237acb9f0730125d229c5
nairol203 <florian@nairol.me>
2025-10-31 14:48:10 +0100

    feat: docker health check

--- next.config.js
+++ next.config.js
@@ -10,6 +10,14 @@ const nextConfig = {
     images: {
         domains: ['i.scdn.co', 'mosaic.scdn.co', 'wrapped-images.spotifycdn.com', 'lineup-images.scdn.co', 'newjams-images.scdn.co', 'scontent-ams4-1.xx.fbcdn.net'],
     },
+    rewrites: () => {
+        return [
+            {
+                source: "/healthz",
+                destination: "/api/health"
+            }
+        ]
+    },
 };
 
 module.exports = nextConfig;
